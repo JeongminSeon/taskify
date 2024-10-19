67521337dc8aa7d02890bb736ab2dae56ec9d1ed
mungyun <mungyun1234@naver.com>
2024-10-19 15:38:02 +0900

    Feat : Landing페이지 반응형 UI 구현

--- pages/Landing/LandingBanner.tsx
+++ pages/Landing/LandingBanner.tsx
@@ -4,19 +4,21 @@ import { useRouter } from "next/router";
 const LandingBanner = () => {
   const router = useRouter();
   return (
-    <div className="pt-[94px] pb-[160px] z-0">
+    <div className="md:pt-[94px] md:pb-[180px] sm:pt-[42px] sm:pb-[76px]">
       <div className="flex flex-col items-center">
         <Image
-          className="mb-12 xl:w-[722px] xl:h-[423px] md:w-[537px] md:h-[315px]"
+          className="lg:w-[722px] lg:h-[423px] md:w-[537px] md:h-[315px] md:mb-12 sm:w-[287px] sm:h-[168px] sm:mb-[26px]"
           src="/images/resource/main_bnr.png"
           alt="메인이미지"
           width="722"
           height="423"
           priority
         />
-        <h2 className="text-white text-[76px] font-bold mb-[111px]">
-          새로운 일정 관리{" "}
-          <span className="text-[90px] text-purple100">Taskify</span>
+        <h2 className="flex md:flex-row sm:flex-col sm:items-center lg:gap-7 md:gap-6 text-white100 font-bold lg:text-[76px] lg:mb-[111px] md:text-[56px] md:mb-[109px] sm:text-[40px] sm:mb-[101px] ">
+          새로운 일정 관리 <br className="md:hidden" />
+          <span className="text-purple100 lg:text-[90px] md:text-[70px] sm:text-[42px]">
+            Taskify
+          </span>
         </h2>
         <button
           className="px-[101px] py-[9px] rounded-lg text-white100 text-[18px] font-[500] bg-purple100"
--- pages/Landing/LandingInfo.tsx
+++ pages/Landing/LandingInfo.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { infoHeadingStyle, infoSectionStyle, infoTextStyle } from "./styles";
 
 interface CardProps {
   imageSrc: string;
@@ -10,10 +11,17 @@ interface CardProps {
 const Card = ({ imageSrc, imageAlt, title, description }: CardProps) => {
   return (
     <div>
-      <div className="flex items-center justify-center bg-gray100 w-[378px] h-[260px] rounded-t-lg">
-        <Image src={imageSrc} alt={imageAlt} width="300" height="124" />
+      <div className="flex items-center justify-center bg-gray100 md:w-[378px] md:h-[260px] sm:w-[343px] sm:h-[236px] rounded-t-lg">
+        <Image
+          className="lg:w-[300px] md:w-[378px] sm:w-[260px]"
+          src={imageSrc}
+          alt={imageAlt}
+          width="300"
+          height="124"
+          style={{ width: "auto", height: "auto" }}
+        />
       </div>
-      <div className="flex gap-[18px] justify-center flex-col bg-black200 w-[378px] h-[124px] rounded-b-lg pl-8">
+      <div className="flex gap-[18px] justify-center flex-col bg-black200 md:w-[378px] md:h-[124px] sm:w-[343px] sm:h-[113px] rounded-b-lg pl-8">
         <span className="text-[18px] font-bold">{title}</span>
         <p className="text-[16px] font-[500px]">{description}</p>
       </div>
@@ -22,24 +30,19 @@ const Card = ({ imageSrc, imageAlt, title, description }: CardProps) => {
 };
 
 const LandingInfo = () => {
-  // 공통 스타일 변수
-  const sectionClass =
-    "flex justify-between items-end bg-black200 rounded-lg text-white100 h-[600px] mb-[90px]";
-  const textClass = "text-[22px] font-[500px] text-gray300";
-  const headingClass = "text-[48px] font-bold mt-[100px]";
-
   return (
     <div>
-      <div className={sectionClass}>
-        <div className="ml-[60px] mb-[223px]">
-          <span className={textClass}>Point 1</span>
-          <h2 className={headingClass}>
+      <div className={infoSectionStyle}>
+        <div className="flex flex-col md:ml-[60px] lg:mb-[223px] md:mt-[63px] sm:mt-[60px] md:items-start sm:items-center ">
+          <span className={infoTextStyle}>Point 1</span>
+          <h2 className={`${infoHeadingStyle} md:text-left sm:text-center`}>
             일의 <span className="text-gray300">우선순위</span>를
             <br />
             관리하세요
           </h2>
         </div>
         <Image
+          className="lg:w-[594px] lg:h-[497px] md:w-[519px] md:h-[435px] md:ml-[145px] sm:w-[296px] sm:h-[248px] sm:ml-[47px]"
           src="/images/resource/landing1.png"
           alt="렌딩이미지1"
           width="594"
@@ -47,17 +50,17 @@ const LandingInfo = () => {
         />
       </div>
 
-      <div className={sectionClass}>
+      <div className={infoSectionStyle}>
         <Image
-          className="ml-[108px]"
+          className="lg:ml-[108px] lg:w-[436px] lg:h-[502px] md:w-[360px] md:h-[415px] sm:w-[217px] sm:h-[250px] lg:order-1 sm:order-2 md:m-auto md:mb-0"
           src="/images/resource/landing2.png"
           alt="렌딩이미지2"
           width="436"
           height="502"
         />
-        <div className="mr-[326px] mb-[223px]">
-          <span className={textClass}>Point 2</span>
-          <h2 className={headingClass}>
+        <div className="lg:mr-[326px] lg:mb-[223px] md:ml-[60px] md:mt-[63px] sm:mt-[60px]  lg:order-2 sm:order-1">
+          <span className={infoTextStyle}>Point 2</span>
+          <h2 className={infoHeadingStyle}>
             해야 할 일을
             <br />
             등록하세요
@@ -65,11 +68,11 @@ const LandingInfo = () => {
         </div>
       </div>
 
-      <div className="mb-40 text-white100">
-        <h3 className="text-white100 text-[28px] font-bold mb-9">
+      <div className="md:mb-40 sm:mb-[120px] text-white100 flex flex-col lg:items-start sm:items-center">
+        <h3 className="text-white100 md:text-[28px] font-bold mb-9">
           생산성을 높이는 다양한 설정 ⚡
         </h3>
-        <div className="flex gap-[33px]">
+        <div className="flex items-center lg:gap-[33px] md:gap-12 sm:gap-10 lg:flex-row sm:flex-col">
           <Card
             imageSrc="/images/resource/landing3.png"
             imageAlt="렌딩이미지3"
