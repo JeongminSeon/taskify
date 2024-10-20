abb16485271f81bd4c7594e92e47994e2f97a1cd
JeongminSeon <skelters@naver.com>
2024-10-20 22:58:58 +0900

    #10 상태 기반으로 유효성 검사 기능 구현 및 잘못 입력했을 시 css 구성

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,10 +8,50 @@ import visibilityOn from "@/public/images/icons/icon_visibility.svg?url";
 const Login = () => {
   const [isShowPW, setIsShwoPW] = useState(false);
 
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    identifier: string
+  ) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  };
+
+  const handleBlurChange = (identifier: string) => {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  };
+
   const handleShowPW = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setIsShwoPW((prev) => !prev);
   };
+
+  const isEmailNotValid =
+    didEdit.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(enteredValues.email);
+
+  const isPWNotValid =
+    didEdit.password && !(enteredValues.password.length >= 8);
+
+  const emailFail = isEmailNotValid ? "border-red100 border-1" : "";
+  const pwFail = isPWNotValid ? "border-red100 border-1" : "";
 
   return (
     <div className='w-full h-full mx-auto md:max-w-[520px] sm:max-w-[351px] flex flex-col gap-3 justify-center items-center'>
@@ -22,18 +62,29 @@ const Login = () => {
       <form className='flex flex-col w-full gap-3'>
         <label htmlFor='email'>이메일</label>
         <input
-          className='w-full border px-4 py-3 border-gray400 rounded-lg focus:outline-none focus : ring-0 focus:border-purple100'
+          className={`w-full border px-4 py-3 border-gray400 rounded-lg focus:outline-none focus:ring-0 focus:border-purple100 ${emailFail}`}
           id='email'
           type='email'
           placeholder='이메일을 입력해 주세요.'
+          onChange={(event) => handleInputChange(event, "email")}
+          onBlur={() => handleBlurChange("email")}
+          value={enteredValues.email}
         />
+        {isEmailNotValid && (
+          <div className='text-sm text-red100'>
+            이메일 형식으로 작성해 주세요.
+          </div>
+        )}
         <label htmlFor='password'>비밀번호</label>
         <div className='flex items-center relative'>
           <input
-            className='w-full border px-4 py-3 border-gray400 rounded-lg focus:outline-none focus : ring-0 focus:border-purple100'
+            className={`w-full border px-4 py-3 border-gray400 rounded-lg focus:outline-none focus:ring-0 focus:border-purple100 ${pwFail}`}
             id='password'
             type={isShowPW ? "text" : "password"}
             placeholder='비밀번호를 입력해주세요'
+            onChange={(event) => handleInputChange(event, "password")}
+            onBlur={() => handleBlurChange("password")}
+            value={enteredValues.password}
           />
           <button
             className='absolute right-3 cursor-pointer '
@@ -45,6 +96,9 @@ const Login = () => {
             />
           </button>
         </div>
+        {isPWNotValid && (
+          <div className='text-sm text-red100'>8자 이상 입력해 주세요.</div>
+        )}
         <button className='bg-gray300 py-3 rounded-lg text-white text-lg mt-2'>
           로그인
         </button>
