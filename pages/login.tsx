a7040f3cc8226f977929b7a07d764cab9c22c345
JeongminSeon <skelters@naver.com>
2024-10-20 14:14:39 +0900

    #10 svg 이미지를 url로 사용하기 위한 환경 설정

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logoImage from "@/public/images/logos/logo-main.svg";
+
+const Login = () => {
+  return (
+    <div>
+      <Image src={logoImage} width={500} height={500} alt='logo_main' />
+    </div>
+  );
+};
+
+export default Login;
