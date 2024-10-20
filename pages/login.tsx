782c47af6315284cf80c6923389e8dc6b790703e
JeongminSeon <skelters@naver.com>
2024-10-20 14:38:13 +0900

    #10 __next height : 100vh 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,8 @@ import logoImage from "@/public/images/logos/logo-main.svg?url";
 
 const Login = () => {
   return (
-    <div>
-      <Image src={logoImage} width={500} height={500} alt='logo_main' />
+    <div className='w-full h-full mx-auto md:max-w-[744px] sm:max-w-[375px] flex justify-center items-center'>
+      <Image src={logoImage} width={100} height={100} alt='logo_main' />
     </div>
   );
 };
