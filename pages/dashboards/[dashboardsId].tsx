6e37e7ae8023cd41a08b61e8ffefc210569b2236
yyezzzy <t47047877@gmail.com>
2024-10-23 14:16:28 +0900

    feat: 새로운 칼럼 추가하기 기능 추가

--- pages/dashboards/[dashboardsId].tsx
+++ pages/dashboards/[dashboardsId].tsx
@@ -1,34 +1,66 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { getColumns } from "../api/columnsApi";
+import { createColumn, getColumns } from "../api/columnsApi";
 import { ColoumnsParams, Columns, ColumnsResponse } from "@/types/columns";
 import Image from "next/image";
 import Column from "@/components/DashBoard/Column";
 import DashBoardLayout from "@/components/Layout/DashBoardLayout";
+import Portal from "@/components/UI/Modal/ModalPotal";
+import OneInputModal from "@/components/UI/Modal/InputModal/OneInputModal";
 
 const DashboardDetail: React.FC = () => {
+  const teamId: string = "9-1";
   const router = useRouter();
   const { dashboardsId } = router.query;
   const [columns, setColumns] = useState<Columns[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleAddColumn = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirm = async (inputValue: string) => {
+    alert(inputValue);
+    setIsModalOpen(false);
+    const newColumn = await createColumn({
+      teamId,
+      title: inputValue,
+      dashboardId: Number(dashboardsId),
+    });
+
+    if (newColumn) {
+      setColumns((prev) => [
+        ...prev,
+        { ...newColumn, teamId, dashboardId: Number(dashboardsId) },
+      ]);
+    }
+    // 샐행 후 컬럼 목록을 다시 불러오는 코드 작성
+    fetchColumns();
+  };
+
+  // 컬럼 목록 조회
+  const fetchColumns = async () => {
+    const dashboardId = Number(dashboardsId); // dashboardsId 숫자로 변환
+    const params: ColoumnsParams = { teamId, dashboardId };
+
+    try {
+      const columnsData: ColumnsResponse = await getColumns(params);
+      setColumns(columnsData.data);
+    } catch (err) {
+      console.error("Error fetching columns:", err);
+      setError("Failed to fetch columns. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchColumns = async () => {
-      const dashboardId = Number(dashboardsId); // dashboardsId 숫자로 변환
-      const params: ColoumnsParams = { teamId: "9-1", dashboardId };
-
-      try {
-        const columnsData: ColumnsResponse = await getColumns(params);
-        setColumns(columnsData.data);
-      } catch (err) {
-        console.error("Error fetching columns:", err);
-        setError("Failed to fetch columns. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     if (dashboardsId) {
       fetchColumns();
     }
@@ -48,6 +80,7 @@ const DashboardDetail: React.FC = () => {
           <button
             type="button"
             className="flex justify-center items-center gap-3 w-full sm:h-[66px] h-[70px] lg:mt-12 border border-gray400 rounded-md bg-white100 text-black300 font-bold"
+            onClick={handleAddColumn}
           >
             새로운 컬럼 추가하기
             <Image
@@ -58,6 +91,18 @@ const DashboardDetail: React.FC = () => {
             />
           </button>
         </div>
+        <Portal>
+          <OneInputModal
+            isOpen={isModalOpen}
+            modalTitle="새 칼럼 생성"
+            inputLabel="이름"
+            inputPlaceholder="컬럼 이름을 입력해주세요"
+            onCancle={closeModal}
+            cancleButtonText="취소"
+            onConfirm={handleConfirm}
+            confirmButtonText="생성"
+          />
+        </Portal>
       </div>
     </DashBoardLayout>
   );
