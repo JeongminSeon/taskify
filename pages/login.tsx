f9566da946106addbc65911dcc063c8ee985759b
yyezzzy <t47047877@gmail.com>
2024-10-23 21:31:45 +0900

    fix: login 페이지 사용되지 않는 event 매개변수를 제거

--- pages/login.tsx
+++ pages/login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
     hasError: (value) => isPWValid(value),
   });
 
-  const handleShowPW = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleShowPW = () => {
     setIsShwoPW((prev) => !prev);
   };
 
@@ -45,17 +45,17 @@ const Login = () => {
   };
 
   return (
-    <div className='w-full h-full mx-auto md:max-w-[520px] sm:max-w-[351px] flex flex-col gap-3 justify-center items-center'>
-      <div className='flex flex-col items-center gap-1'>
-        <Image src={logoImage} width={200} height={280} alt='logo_main' />
-        <p className='text-xl'>오늘도 만나서 반가워요!</p>
+    <div className="w-full h-full mx-auto md:max-w-[520px] sm:max-w-[351px] flex flex-col gap-3 justify-center items-center">
+      <div className="flex flex-col items-center gap-1">
+        <Image src={logoImage} width={200} height={280} alt="logo_main" />
+        <p className="text-xl">오늘도 만나서 반가워요!</p>
       </div>
-      <form className='flex flex-col w-full gap-3' onSubmit={handleSubmit}>
+      <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
         <Input
-          id='email'
-          type='email'
-          placeholder='이메일을 입력해 주세요.'
-          label='이메일'
+          id="email"
+          type="email"
+          placeholder="이메일을 입력해 주세요."
+          label="이메일"
           onChange={(event) => handleEmailInputChange(event)}
           onBlur={handleEmailBlurChange}
           value={emailValue}
@@ -63,10 +63,10 @@ const Login = () => {
           error={isEmailNotValid ? "이메일 형식으로 작성해 주세요." : ""}
         />
         <Input
-          id='password'
+          id="password"
           type={isShowPW ? "text" : "password"}
-          placeholder='비밀번호를 입력해 주세요.'
-          label='비밀번호'
+          placeholder="비밀번호를 입력해 주세요."
+          label="비밀번호"
           onChange={(event) => handlePWInputChange(event)}
           onBlur={handlePWBlurChange}
           value={passwordValue}
@@ -76,8 +76,8 @@ const Login = () => {
           onClick={handleShowPW}
         />
         <button
-          className='bg-gray300 py-3 rounded-lg text-white text-lg mt-2'
-          type='submit'
+          className="bg-gray300 py-3 rounded-lg text-white text-lg mt-2"
+          type="submit"
         >
           로그인
         </button>
@@ -86,8 +86,8 @@ const Login = () => {
         <p>
           회원이 아니신가요?
           <Link
-            href='/signup'
-            className='ml-2 text-purple100 underline underline-offset-4'
+            href="/signup"
+            className="ml-2 text-purple100 underline underline-offset-4"
           >
             회원가입하기
           </Link>
