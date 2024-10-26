13b4fb89e36f6bfa4e9b3a1b14b50ef074d2da04
JeongminSeon <skelters@naver.com>
2024-10-27 00:30:19 +0900

    #51 Input 공통 컴포넌트에 labelStyle prop 추가

--- components/MyDashBoard/CreateDashBoard.tsx
+++ components/MyDashBoard/CreateDashBoard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Portal from "../UI/modal/ModalPotal";
 import ModalLayout from "../Layout/ModalLayout";
+import Input from "../Auth/Input";
+import useInput from "@/hooks/useInput";
+import { isEntered } from "@/utils/validation";
 
 interface DashBoardProps {
   isOpen: boolean;
@@ -10,6 +13,15 @@ interface DashBoardProps {
 const CreateDashBoard = ({ isOpen, onClose }: DashBoardProps) => {
   if (!isOpen) return null;
 
+  const {
+    enteredValue: nameValue,
+    handleInputChange,
+    handleBlurChange,
+    error,
+  } = useInput({
+    defaultValue: "",
+    hasError: (enteredValue: string) => isEntered(enteredValue),
+  });
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
@@ -17,11 +29,21 @@ const CreateDashBoard = ({ isOpen, onClose }: DashBoardProps) => {
   return (
     <Portal>
       <ModalLayout>
-        <div className="bg-white rounded-2xl md:w-[584px] p-5">
-          <h2>새로운 대시보드 생성</h2>
-          <form onSubmit={handleSubmit}>
-            <h3>대시보드 이름</h3>
-            <div className="flex gap-1">
+        <div className="bg-white rounded-2xl md:w-[584px] p-5 flex flex-col gap-3">
+          <h2 className="text-black text-2xl font-bold">새로운 대시보드</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-5">
+            <Input
+              id="name"
+              label="대시보드 이름"
+              onBlur={handleBlurChange}
+              onChange={(event) => handleInputChange(event)}
+              value={nameValue}
+              isPassword={false}
+              placeholder=""
+              type="text"
+              labelStyle="text-xl font-bold"
+            />
+            <div className="flex gap-2">
               <li>그린</li>
               <li>블루</li>
               <li>주황</li>
@@ -29,9 +51,19 @@ const CreateDashBoard = ({ isOpen, onClose }: DashBoardProps) => {
               <li>핑크</li>
             </div>
           </form>
-          <div>
-            <button onClick={onClose}>취소</button>
-            <button onClick={onClose}>생성</button>
+          <div className="flex justify-between gap-3 mt-5">
+            <button
+              className="border w-full py-3.5 px-11 rounded-lg text-lg"
+              onClick={onClose}
+            >
+              취소
+            </button>
+            <button
+              className="border w-full py-3.5 px-11 rounded-lg text-lg text-white border-purple200 bg-purple200"
+              onClick={onClose}
+            >
+              생성
+            </button>
           </div>
         </div>
       </ModalLayout>
