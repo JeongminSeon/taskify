32a3a7c6d14e8cb06abef2fa6d563373cf96dae9
JeongminSeon <skelters@naver.com>
2024-10-27 00:04:34 +0900

    #51 tailwind 색상 추가

--- components/MyDashBoard/CreateDashBoard.tsx
+++ components/MyDashBoard/CreateDashBoard.tsx
@@ -10,11 +10,25 @@ interface DashBoardProps {
 const CreateDashBoard = ({ isOpen, onClose }: DashBoardProps) => {
   if (!isOpen) return null;
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <Portal>
       <ModalLayout>
-        <div>
-          <div>새로운 대쉬보드 생성</div>
+        <div className="bg-white rounded-2xl md:w-[584px] p-5">
+          <h2>새로운 대시보드 생성</h2>
+          <form onSubmit={handleSubmit}>
+            <h3>대시보드 이름</h3>
+            <div className="flex gap-1">
+              <li>그린</li>
+              <li>블루</li>
+              <li>주황</li>
+              <li>파랑</li>
+              <li>핑크</li>
+            </div>
+          </form>
           <div>
             <button onClick={onClose}>취소</button>
             <button onClick={onClose}>생성</button>
