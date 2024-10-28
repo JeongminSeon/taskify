4bc336f2b9e92701261a40a1af87dd1368965dca
mungyun <mungyun1234@naver.com>
2024-10-28 17:19:35 +0900

    Refactor: Card 컴포넌트 분리

--- components/Cards/Card.tsx
+++ components/Cards/Card.tsx
@@ -1,8 +1,9 @@
 // Card.tsx
 import React from "react";
-import Image from "next/image";
 import { Card as CardType } from "@/types/cards";
-import { hexToRgba } from "@/utils/TodoForm";
+import CardImage from "./CardImage";
+import CardTags from "./CardTags";
+import CardDueDate from "./CardDueDate";
 
 interface CardProps {
   card: CardType;
@@ -11,63 +12,21 @@ interface CardProps {
   dashboardId: number;
 }
 
-const Card: React.FC<CardProps> = ({
-  card,
-  tagColors,
-  onClick,
-  dashboardId,
-}) => (
+const Card: React.FC<CardProps> = ({ card, tagColors, onClick }) => (
   <button
-    className="w-full md:flex md:gap-5 lg:block p-3 border border-gray400 rounded-md bg-white100"
+    className="w-full p-3 border rounded-md bg-white border-gray400 lg:block md:flex md:gap-5"
     onClick={onClick}
   >
-    <div className="overflow-hidden relative w-full h-40 md:flex-[0_0_90px] lg:flex-1 md:h-[53px] lg:h-40 rounded-md">
-      <Image
-        src={card.imageUrl || "/images/resource/card_image1.png"}
-        className="object-cover"
-        fill
-        alt="카드 이미지"
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-      />
-    </div>
-    <div className="w-full pt-1 md:pt-0 lg:pt-[15px]">
-      <h2 className="sm:text-sm md:text-[16px] font-medium text-left">
+    <CardImage imageUrl={card.imageUrl} />
+    <div className="w-full pt-1 lg:pt-[15px] md:pt-0">
+      <h2 className="text-left font-medium sm:text-sm md:text-[16px]">
         {card.title}
       </h2>
-      <div className="md:flex lg:block gap-4">
-        <ul className="tags flex items-center gap-[6px] mt-[6px]">
-          {card.tags.map((tag) => (
-            <li
-              key={tag}
-              className="tag py-1 px-[6px] rounded"
-              style={{
-                backgroundColor: hexToRgba(tagColors[tag] || "#000000", 0.2),
-                color: tagColors[tag] || "#000000",
-              }}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-        <div className="md:flex-1 flex justify-between items-center pt-[6px]">
-          <p className="flex items-center text-gray200 text-xs font-medium">
-            <span>
-              <Image
-                src="/images/icons/icon_calendar.svg"
-                width={18}
-                height={18}
-                alt="캘린더"
-              />
-            </span>
-            {new Date(card.dueDate)
-              .toLocaleDateString("ko-KR", {
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-              })
-              .replace(/\.$/, "")}
-          </p>
-          <p className="overflow-hidden relative w-[34px] h-[34px] rounded-full bg-slate-500">
+      <div className="gap-4 md:flex lg:block">
+        <CardTags tags={card.tags} tagColors={tagColors} />
+        <div className="flex items-center justify-between pt-[6px] md:flex-1">
+          <CardDueDate dueDate={card.dueDate} />
+          <p className="relative w-[34px] h-[34px] overflow-hidden bg-slate-500 rounded-full">
             {card.assignee.nickname}
           </p>
         </div>
