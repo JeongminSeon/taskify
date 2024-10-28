10575455dd8ea33522384854f2ba3e31ad560aef
99uminji <rnalswl96@naver.com>
2024-10-28 16:46:11 +0900

    Test : authstore 초기 상태 api 호출

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { parseCookie } from "@/utils/api/cookie";
+import { getUserInfo } from "@/utils/api/authApi";
 
 // 사용자 정보 구조를 정의
 interface User {
@@ -25,6 +27,27 @@ export const useAuthStore = create<AuthState>((set) => ({
   //  스토어의 초기 상태를 정의
   isAuthenticated: false, //사용자가 인증되었는지를 나타내는 boolean 값
   user: null, // 현재 로그인한 사용자에 대한 정보를 담고 있는 객체
+
+  // 초기 상태일 때 저장된 유저 정보 가져오기
+  initialize: () => {
+    const cookieToken = parseCookie()["access-token"];
+    console.log(cookieToken);
+    if (cookieToken) {
+      // 인증 상태를 true로 설정
+      console.log(cookieToken);
+      set({ isAuthenticated: true });
+      // 사용자 정보를 가져오는 API 호출
+      getUserInfo()
+        .then((user) => {
+          set({ user }); // 사용자 정보 상태 업데이트
+          console.log("업데이트된 상태:", useAuthStore.getState());
+        })
+        .catch((error) => {
+          console.error("사용자 정보를 가져오지 못했습니다:", error);
+        });
+    }
+  },
+
   // setUser : 사용자 정보와 인증 상태를 업데이트합니다
   setUser: (user: User) => {
     console.log("setUser 호출됨:", user);
--- utils/api/cookie.ts
+++ utils/api/cookie.ts
@@ -1,5 +1,12 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+
+// 쿠키에서 토큰을 읽어오는 유틸리티 함수
+export const parseCookie = () => {
+  return {
+    "access-token": Cookies.get("accessToken"), // 쿠키에서 access-token 가져오기
+  };
+};
 
 export const setAccessToken = (token: string) => {
-  Cookies.set('accessToken', token, { expires: 1 }); // 유효기간 1일
+  Cookies.set("accessToken", token, { expires: 1 }); // 유효기간 1일
 };
