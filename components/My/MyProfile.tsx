957fa3afdf1828edee32e5abfc25dcfb3491518b
mungyun <mungyun1234@naver.com>
2024-10-29 13:29:17 +0900

    Feat: MyProfile 유저 정보 로딩 추가

--- components/My/MyProfile.tsx
+++ components/My/MyProfile.tsx
@@ -1,25 +1,51 @@
 import Image from "next/image";
 import InputField from "./InputField";
 import MyButton from "./MyButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useModal from "@/hooks/modal/useModal";
 import ModalAlert from "../UI/modal/ModalAlert";
+import { getUserInfo } from "@/utils/api/authApi";
+
+interface Profile {
+  email: string;
+  nickname: string;
+  profileImageUrl: string | null;
+}
 
 const MyProfile: React.FC = () => {
-  const [nickname, setNickname] = useState<string>("");
-  const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [profile, setProfile] = useState<Profile>({
+    email: "",
+    nickname: "",
+    profileImageUrl: null,
+  });
   const { isOpen, openModal, closeModal } = useModal();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
-      setProfileImage(e.target.files[0]);
+      console.log("이미지 수정");
     }
   };
 
   const handleSave = () => {
     openModal();
-    console.log("닉네임:", nickname, "이미지:", profileImage);
   };
+
+  const getMe = async () => {
+    try {
+      const res = await getUserInfo();
+      setProfile({
+        email: res.email,
+        nickname: res.nickname,
+        profileImageUrl: res.profileImageUrl,
+      });
+    } catch (error) {
+      console.error("프로필 정보를 불러오는 중 오류가 발생했습니다:", error);
+    }
+  };
+
+  useEffect(() => {
+    getMe();
+  }, []);
 
   return (
     <div className="lg:w-[672px] md:w-[548px] sm:w-[284px] md:p-6 sm:p-4 md:mb-6 sm:mb-4 rounded-2xl bg-white100">
@@ -31,8 +57,10 @@ const MyProfile: React.FC = () => {
           <label htmlFor="inputFile">
             <Image
               className="cursor-pointer"
-              src="/images/icons/profile_add_box.svg"
-              alt="파일추가"
+              src={
+                profile.profileImageUrl || "/images/icons/profile_add_box.svg"
+              }
+              alt="프로필 이미지"
               width="182"
               height="182"
             />
@@ -50,15 +78,17 @@ const MyProfile: React.FC = () => {
               label="이메일"
               name="email"
               type="email"
-              placeholder="1212@naver.com"
+              value={profile.email}
               readOnly
             />
             <InputField
               label="닉네임"
               name="nickname"
               type="text"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
+              value={profile.nickname}
+              onChange={(e) =>
+                setProfile({ ...profile, nickname: e.target.value })
+              }
             />
           </div>
           <MyButton onClick={handleSave}>저장</MyButton>
