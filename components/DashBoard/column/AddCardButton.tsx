4af443ab13b34d7d046ceb00297c3a37ae7d933b
mungyun <mungyun1234@naver.com>
2024-10-30 17:04:58 +0900

    Feat: AddCardButton 컴포넌트 추가

---
+++ components/DashBoard/column/AddCardButton.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const AddCardButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button
+      type="button"
+      className="block w-full h-8 md:h-10 mt-6 border border-gray400 rounded-md bg-white100"
+      onClick={onClick}
+    >
+      <Image
+        src="/images/icons/icon_add_column.svg"
+        width={16}
+        height={16}
+        alt="할 일 추가"
+        className="mx-auto"
+      />
+    </button>
+  );
+};
+
+export default AddCardButton;
