e13dd8dfd9ab53d19164d75818e8ec4af313c982
mungyun <mungyun1234@naver.com>
2024-10-30 20:35:31 +0900

    Refactor: My페이지 프로필 SSR 로직 적용

--- pages/my/index.tsx
+++ pages/my/index.tsx
@@ -2,12 +2,36 @@ import DashBoardLayout from "@/components/Layout/DashBoardLayout";
 import MyPassWord from "@/components/My/MyPassWord";
 import MyProfile from "@/components/My/MyProfile";
 import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
+import { getUserInfo } from "@/utils/api/authApi";
+import { ProfileProps } from "@/types/my";
+import { parseCookies } from "nookies";
 
-const MyPage = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = parseCookies(context); // 쿠키 파싱
+
+  try {
+    // 쿠키에서 필요한 인증 토큰을 가져오기
+    const accessToken = cookies.accessToken; // 쿠키 이름에 맞게 수정
+
+    const profileData = await getUserInfo(accessToken); // 서버에서 프로필 데이터 가져오기
+    return {
+      props: { profileData }, // 데이터를 props로 전달
+    };
+  } catch (error) {
+    console.error("프로필 정보를 불러오는 중 오류 발생했습니다.", error);
+    return {
+      props: { profileData: null }, // 오류 발생 시 null 전달
+    };
+  }
+};
+
+const MyPage = ({ profileData }: { profileData: ProfileProps }) => {
   const router = useRouter();
   const returnButton = () => {
     router.back();
   };
+
   return (
     <DashBoardLayout>
       <div className="mt-5 ml-5 md:mb-[136px] sm:mb-[61px] min-h-screen">
@@ -17,7 +41,7 @@ const MyPage = () => {
         >
           &lt; 돌아가기
         </button>
-        <MyProfile />
+        <MyProfile profileData={profileData} />
         <MyPassWord />
       </div>
     </DashBoardLayout>
