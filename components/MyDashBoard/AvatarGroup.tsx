9300ae861cf31a468f53d2d6fd99672992c1d99a
JeongminSeon <skelters@naver.com>
2024-10-31 12:38:06 +0900

    타입 재정의 및 동적으로 프로필 아바타 생성 구현

--- components/MyDashBoard/AvatarGroup.tsx
+++ components/MyDashBoard/AvatarGroup.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import axiosInstance from "@/utils/api/axiosInstanceApi";
+import React, { useEffect, useState } from "react";
+
+interface Member {
+  id: number;
+  userId: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+  isOwner: boolean;
+}
 
 const AvatarGroup = () => {
-  return <div>아바타 그룹임다.</div>;
+  const [avatars, setAvatars] = useState<Member[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const loadMembers = async () => {
+    try {
+      const response = await axiosInstance.get(
+        "/members?page=1&size=20&dashboardId=12006"
+      );
+      setAvatars(response.data.members);
+      setTotalCount(response.data.totalCount);
+    } catch (error) {
+      console.error("예기치 못한 에러가 발생했습니다.", error);
+    }
+  };
+
+  useEffect(() => {
+    loadMembers();
+  }, []);
+
+  return (
+    <div className="flex items-center">
+      {avatars.slice(0, 4).map((member, index) => (
+        <div
+          key={member.id}
+          style={{
+            backgroundColor: member.profileImageUrl ? "transparent" : "#90cdf4", // 이미지가 없을 때 색상 설정
+            backgroundImage: member.profileImageUrl
+              ? `url(${member.profileImageUrl})`
+              : "none", // 이미지가 있을 때만 설정
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            zIndex: avatars.length - index,
+          }}
+          className="w-10 h-10 rounded-full text-white flex items-center justify-center text-lg -ml-3 border-2 border-white"
+        >
+          <p>{member.nickname[0]}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AvatarGroup;
