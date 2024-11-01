9ddd2ba6e7ffdebdc21235a4dbfc1874b80284ba
JeongminSeon <skelters@naver.com>
2024-11-01 23:18:13 +0900

    #90 대쉬보드 색상 표기 오류 수정

--- components/DashBoard/inputs/ColorInput.tsx
+++ components/DashBoard/inputs/ColorInput.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import Image from "next/image";
 import checkImage from "../../../public/images/icons/icon_select.svg?url";
 
+type ColorKey = "green" | "violet" | "orange" | "blue" | "pink";
+
 interface ColorPorps {
-  color: string;
+  color: ColorKey;
   isSelected: boolean;
   onClick: () => void;
 }
+
+const COLOR: Record<ColorKey, string> = {
+  green: "bg-green100",
+  violet: "bg-violet100",
+  orange: "bg-orange100",
+  blue: "bg-blue100",
+  pink: "bg-pink100",
+};
 
 const ColorInput = ({ color, isSelected = true, onClick }: ColorPorps) => {
   return (
     <button
       type="button"
-      className={`p-1 bg-${color}100 rounded-full w-8 h-8`}
+      className={`p-1 ${COLOR[color]} rounded-full w-8 h-8`}
       onClick={onClick}
     >
       {isSelected && (
