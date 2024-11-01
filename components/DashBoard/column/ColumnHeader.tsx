772a32010a78b834e7b9a1bcf8d8327093cf00f4
yyezzzy <t47047877@gmail.com>
2024-11-01 11:12:12 +0900

    Todo: 칼럼 톱니봐키 클릭시 수정, 삭제 구현 예정

--- components/DashBoard/column/ColumnHeader.tsx
+++ components/DashBoard/column/ColumnHeader.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-
+import { useState } from "react";
 const ColumnHeader = ({ title }: { title: string }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleSettingClick = () => {};
   return (
     <h2 className="flex items-center gap-2 text-black100 font-bold">
       <span className="block w-2 h-2 rounded-full bg-purple100"></span>
       <p className="flex-1">{title}</p>
-      <button type="button">
+      <button type="button" onClick={handleSettingClick}>
         <Image
           src="/images/icons/icon_settings.svg"
           width={22}
