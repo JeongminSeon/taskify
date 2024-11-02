94ff2f66b46f27834b66290ca7651dc653665707
mungyun <mungyun1234@naver.com>
2024-11-02 17:19:27 +0900

    Fix: user 할당 이후 사용되지 않는 것 제거

--- components/MyDashBoard/AvatarGroup.tsx
+++ components/MyDashBoard/AvatarGroup.tsx
@@ -11,21 +11,20 @@ const AvatarGroup = () => {
   const router = useRouter();
   const { dashboardsId } = router.query;
 
-  const loadMembers = async () => {
-    if (!dashboardsId) return;
-    try {
-      const response = await getMembers({
-        dashboardId: Number(dashboardsId as string),
-      }); // id를 문자열로 캐스팅
-      setAvatars(response.members);
-      setTotalCount(response.totalCount);
-    } catch (error) {
-      console.error("예기치 못한 에러가 발생했습니다.", error);
-    }
-  };
-
   // id가 변경되었거나 처음 로드되었을 때만 loadMembers 호출
   useEffect(() => {
+    const loadMembers = async () => {
+      if (!dashboardsId) return;
+      try {
+        const response = await getMembers({
+          dashboardId: Number(dashboardsId as string),
+        }); // id를 문자열로 캐스팅
+        setAvatars(response.members);
+        setTotalCount(response.totalCount);
+      } catch (error) {
+        console.error("예기치 못한 에러가 발생했습니다.", error);
+      }
+    };
     if (dashboardsId) {
       loadMembers();
     }
