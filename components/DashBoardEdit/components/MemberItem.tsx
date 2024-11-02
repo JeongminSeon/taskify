5f59bd7db2b7e4a21c88917e8fb868d217cd2cc0
yyezzzy <t47047877@gmail.com>
2024-11-02 17:09:20 +0900

    fix: 충돌 해결

--- components/DashBoardEdit/components/MemberItem.tsx
+++ components/DashBoardEdit/components/MemberItem.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import React from "react";
 
 interface MemberItemProps {
   member: {
     id: number;
     nickname: string;
+    profileImageUrl?: string;
   };
   handleDeleteMember: (id: number) => void;
 }
@@ -17,7 +19,13 @@ const MemberItem: React.FC<MemberItemProps> = ({
       key={member.id}
       className="flex items-center gap-2 md:gap-3 py-3 md:py-4 px-4 md:px-7 border-b border-gray500"
     >
-      <p className="overflow-hidden relative w-[34px] h-[34px] rounded-full bg-slate-500"></p>
+      <Image
+        className="overflow-hidden relative w-[34px] h-[34px] rounded-full"
+        src={member?.profileImageUrl || ""}
+        alt="맴버 프로필"
+        width="38"
+        height="38"
+      />
       <p className="flex-1 text-sm md:text-[16px]">{member.nickname}</p>
       <button
         type="button"
