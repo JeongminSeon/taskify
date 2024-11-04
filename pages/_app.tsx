8375fa7f2507d45261a533213556b3d566fd2621
JeongminSeon <skelters@naver.com>
2024-11-04 12:08:39 +0900

    불필요한 import 문 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useAuthStore } from "@/store/authStore";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
