dbe67b848cd7a875d7701bd0710452528daa70bb
JeongminSeon <skelters@naver.com>
2024-11-04 15:17:13 +0900

    UI, Auth 폴더 컴포넌트 주석처리

--- components/UI/modal/CardModal/components/CardCommentList.tsx
+++ components/UI/modal/CardModal/components/CardCommentList.tsx
@@ -7,6 +7,7 @@ import { CommentInput } from "@/components/Comments/CommentInput";
 import { CommentItem } from "@/components/Comments/CommentItem";
 import ModalAlert from "../../ModalAlert";
 
+// 컴포넌트에 전달되는 props 인터페이스 정의
 interface CommentProps {
   card: Card;
   dashboardId?: number;
@@ -14,24 +15,31 @@ interface CommentProps {
 
 const CardCommentList: React.FC<CommentProps> = ({ card }) => {
   const router = useRouter();
-  const dashboardId = Number(router.query.dashboardsId);
+  const dashboardId = Number(router.query.dashboardsId); // URL에서 대시보드 ID 추출
 
+  // useComments 훅을 사용하여 상태, 모달, 액션 불러오기
   const { state, modal, actions } = useComments({
     card,
     dashboardId,
   });
 
+  // 댓글 상태 값 추출
   const { comments, content, editCommentId, editContent } = state;
+
+  // 모달 상태 및 메세지 추출
   const { isOpen, modalMessage, closeModal } = modal;
+
+  // 댓글 관련 액션들
   const {
-    fetchComments,
-    handleInputChange,
-    handleCommentCreate,
-    handleCommentChange,
-    handleEditClick,
-    handleCommentDelete,
+    fetchComments, // 댓글 가져오기
+    handleInputChange, // 입력 변경 핸들러
+    handleCommentCreate, // 댓글 생성 핸들러
+    handleCommentChange, // 댓글 수정 핸들러
+    handleEditClick, // 댓글 수정 모드 활성화
+    handleCommentDelete, // 댓글 삭제 핸들러
   } = actions;
 
+  // 컴포넌트 마운트 시 댓글 목록 가져오기
   useEffect(() => {
     fetchComments();
   }, [fetchComments]);
